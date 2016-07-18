f06f3c9a2e6e409f77aa06f1ce8e70cf651c7537
amcomaschi <arielcomaschi@gmail.com>
2016-07-18 18:28:12 -0300

    Initial commit

---
+++ app/app.component.ts
@@ -0,0 +1,11 @@
+/**
+ * Created by amcomaschi on 18/07/16.
+ */
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>My First Angular 2 App</h1>'
+})
+
+export class AppComponent { }
---
+++ app/main.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by amcomaschi on 18/07/16.
+ */
+import { bootstrap }    from '@angular/platform-browser-dynamic';
+import { AppComponent } from './app.component';
+
+bootstrap(AppComponent);
