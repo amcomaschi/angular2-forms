760d3175b3756c926a6dde21f503ed66ecac7944
arielcomaschi@gmail.com <arielcomaschi@gmail.com>
2016-07-19 00:29:09 -0300

    Form added.

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,10 +2,12 @@
  * Created by amcomaschi on 18/07/16.
  */
 import { Component } from '@angular/core';
+import { HeroFormComponent } from './hero-form.component';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: '<hero-form></hero-form>',
+    directives: [HeroFormComponent]
 })
 
 export class AppComponent { }
--- app/main.js
+++ app/main.js
@@ -4,5 +4,10 @@
  */
 var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
 var app_component_1 = require('./app.component');
-platform_browser_dynamic_1.bootstrap(app_component_1.AppComponent);
+var forms_1 = require('@angular/forms');
+platform_browser_dynamic_1.bootstrap(app_component_1.AppComponent, [
+    forms_1.disableDeprecatedForms(),
+    forms_1.provideForms()
+])
+    .catch(function (err) { return console.error(err); });
 //# sourceMappingURL=main.js.map
